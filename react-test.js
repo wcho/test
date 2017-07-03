1cbaf2c6595e7021f81c7b3edab75b17c4c59948
WooYoung Cho <wy1.cho@samsung.com>
2017-07-03 13:42:34 +0900

    Update react-test.js

--- react-test.js
+++ react-test.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 class Hello extends React.Component {
     render() {
-        <div>Hello</div>; // BAD_RENDER_RETURN_VALUE alarm because 'render()' does not return this React element.
+        return <div>Hello</div>; // BAD_RENDER_RETURN_VALUE alarm because 'render()' does not return this React element.
     }
 }
